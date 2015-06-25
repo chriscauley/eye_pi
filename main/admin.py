11781b512f390fb679ed850ed3c02b2e56be3f80
chriscauley <chris@lablackey.com>
2015-06-25 13:30:03 -0400

    adding ability to select in admin, removing it from front end

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from .models import Pair
 
 class PairAdmin(admin.ModelAdmin):
-  list_display = ('number','__unicode__','selected',)
+  list_display = ('number','selected','__unicode__',)
   list_editable = ('selected',)
   list_filter = ('selected',)
+  search_fields = ('number',)
 
 admin.site.register(Pair,PairAdmin)
